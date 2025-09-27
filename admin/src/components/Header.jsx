145f086c97e5b0265067f80c69a3dfe42d8ad29f
Lokesh Kashyap <9027410147lk@gmail.com>
2025-09-27 13:15:30 +0530

    styles: improve admin panel design

--- admin/src/components/Header.jsx
+++ admin/src/components/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import {LogOut, ShieldUser} from "lucide-react"
+import {Car, LogOut, ShieldUser} from "lucide-react"
 import style from "../styles/header.module.css"
 
 const Header = () => {
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("reftoken");
-    localStorage.removeItem("reftoken");
     window.location.href = "/login"
   }
   return (
     <div className={style.header}> 
       <div className={style.headerElement}>
-        <ShieldUser className={style.icon}/>
-        <h1>Admin</h1>
+        <Car className={style.icon} />
+        <h1 className={style.heading}>cabXpress</h1>
       </div>
-      <div>
+      <div className={style.bottons}>
         <button onClick={logout} className={style.logoutbtn}>Logout<LogOut/></button>
+      <div className={style.profile}>L</div>
       </div>
     </div>
   )
